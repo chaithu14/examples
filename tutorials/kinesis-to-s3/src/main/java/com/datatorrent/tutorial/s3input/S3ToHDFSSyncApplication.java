ed590db183aa5111462a29bcd73bcdf99a43d7fb
chaitanya <chaithu@apache.org>
2017-03-31 12:30:01 +0530

    Added

--- tutorials/kinesis-to-s3/src/main/java/com/datatorrent/tutorial/s3input/S3ToHDFSSyncApplication.java
+++ tutorials/kinesis-to-s3/src/main/java/com/datatorrent/tutorial/s3input/S3ToHDFSSyncApplication.java
@@ -1,11 +1,11 @@
 package com.datatorrent.tutorial.s3input;
-
-import org.apache.apex.malhar.lib.fs.s3.S3BytesOutputModule;
 import org.apache.hadoop.conf.Configuration;
 
 import com.datatorrent.api.DAG;
 import com.datatorrent.api.StreamingApplication;
 import com.datatorrent.api.annotation.ApplicationAnnotation;
+import com.datatorrent.contrib.kinesis.KinesisStringInputOperator;
+import com.datatorrent.lib.io.ConsoleOutputOperator;
 
 /**
  * Simple application illustrating file copy from S3
@@ -16,9 +16,10 @@ public class S3ToHDFSSyncApplication implements StreamingApplication
   @Override
   public void populateDAG(DAG dag, Configuration conf)
   {
-    ByteArrayGenerator inputModule = dag.addOperator("KinesisInput", new ByteArrayGenerator());
-    //KinesisByteArrayInputOperator inputModule = dag.addOperator("KinesisInput", new KinesisByteArrayInputOperator());
-    S3BytesOutputModule outputModule = dag.addModule("S3OutputModule", new S3BytesOutputModule());
+    //ByteArrayGenerator inputModule = dag.addOperator("KinesisInput", new ByteArrayGenerator());
+    KinesisStringInputOperator inputModule = dag.addOperator("KinesisInput", new KinesisStringInputOperator());
+    //S3BytesOutputModule outputModule = dag.addModule("S3OutputModule", new S3BytesOutputModule());
+    ConsoleOutputOperator outputModule = dag.addOperator("Console", new ConsoleOutputOperator());
 
     dag.addStream("KinesisToS3", inputModule.outputPort, outputModule.input);
   }
